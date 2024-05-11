a798ff04bf05ebdf58dfcf9567d73e87ec93f139
Jonathan Hartman <j@hartman.io>
2024-05-10 22:32:21 -0700

    Add a script to validate the config.yml
    It's called "JSON Resume Schema" but works just as well pulling from YAML.
    Can't run it in a GitHub action yet, though, because of some issues:
    
    - No support for a project_info section. I don't use the URL (yet) but do use
      the language in `index.njk`
    - Start and end dates need to be strings. Easy fix and should do anyway.
    - Only recognizes strings for job location fields and I have them as objects
      to make them compatible with the microformat on the other end.
    
    Can I override the behavior of the schema validator? Offer to submit a change
    upstream at least for the location fields?

---
+++ test/validate_json_resume_schema.js
@@ -0,0 +1,15 @@
+const fs = require("fs");
+const resumeSchema = require("@averagehelper/resume-schema");
+const yaml = require("js-yaml");
+const resume = yaml.load(fs.readFileSync("config/config.yml", "utf-8"));
+
+const validate = async function() {
+  try {
+    await resumeSchema.validate(resume);
+  console.log("Resume data validated successfully");
+  } catch (error) {
+    console.error("Resume data was invalid", error);
+  }
+};
+
+validate();
