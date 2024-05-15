8eed83df1968f0e092d656080aaf601bc91c2478
Jonathan Hartman <j@hartman.io>
2024-05-14 23:49:05 -0700

    Change work and education locations into strings
    The spec we're using expects this and won't validate otherwise. I ought to ask if they'd accept a PR to make these locations potentially maps since the personal location already is. In the meantime...
    
    `Resume data validated successfully`
    
    yeeeaahhhh

--- .eleventy.js
+++ .eleventy.js
@@ -40,21 +40,24 @@ module.exports = function(eleventyConfig) {
 	iconmap.set("email", faSvgCore.findIconDefinition({ prefix: "fas", iconName: "envelope" }));
 	iconmap.set("phone", faSvgCore.findIconDefinition({ prefix: "fas", iconName: "phone" }));
 
+	// Merge the email address in with the other contact links.
 	resumeData.basics.profiles.unshift({
 		network: "email",
 		username: resumeData.basics.email,
 		url: `mailto:${resumeData.basics.email}`
 	});
 
+	// Set an icon for the location.
 	resumeData.basics.location.icon = faSvgCore.icon(
 		iconmap.get("location"),
 		{
 			title: "My location",
-			transform: {size: 22 }
+			transform: { size: 22 }
 		}
 	).html;
 
-	for ( profile of resumeData.basics.profiles ) {
+	// Set an icon for all the contact links.
+	for (profile of resumeData.basics.profiles) {
 		let lwr = profile.network.toLowerCase();
 		let icon = iconmap.get(lwr);
 		profile.icon = faSvgCore.icon(
@@ -65,7 +68,38 @@ module.exports = function(eleventyConfig) {
 			}
 		).html;
 	}
+
+	// Split apart location strings into their contingent city+state+country.
+	for (job of resumeData.work) {
+		let location = job.location;
+
+		// "Remote" is a special case.
+		if (location == "Remote") {
+			continue;
+		}
+
+		const locs = location.split(",").map(i => i.trim());
+		job.location = new Map();
+		job.location.locality = locs[0];
+		job.location.region = locs[1];
+		job.location.countryCode = locs[2];
+	}
 	
+	// Do the same for education entries.
+	for (edu of resumeData.education) {
+		let location = edu.location;
+
+		if (location == "Remote") {
+			continue;
+		}
+
+		const locs = location.split(",").map(i => i.trim());
+		edu.location = new Map();
+		edu.location.locality = locs[0];
+		edu.location.region = locs[1];
+		edu.location.countryCode = locs[2];
+	}
+
 	eleventyConfig.addGlobalData("resumeConfig", resumeConfig);
 	eleventyConfig.addGlobalData("resumeData", resumeData);
 
